f86e8ec5125138ae8011f8819d3ef8c9800d2dfe
kidde60 <kgeorgewilliam60.com>
2023-01-18 14:49:37 +0300

    Add card details

---
+++ src/Projects/CardContainer.js
@@ -0,0 +1,30 @@
+import hotel from '../Assets/hotel.png'
+import bookstore from '../Assets/bookstore.png'
+import todolist from '../Assets/todolist.png'
+const CardContainer = [
+    {
+        imagesrc: hotel,
+        title: 'Hotel webpage',
+        desc: 'This Hotel webpage allows a user to see all the services offered at the hotel, view the available accomodation rooms, their sizes and contact the management for booking a room a rated per night',
+        live: 'https://kidde60.github.io/Capstone-project-1/index.html',
+        source: 'https://github.com/kidde60/Capstone-project-1'
+
+    },
+    {
+        imagesrc: bookstore,
+        title: 'Book store Webpage',
+        desc: 'This book store web app allows a user to add a new book with the author name, remove a book, see all the boooks available in the library and alerts the user incase there are no books in the library.',
+        live: 'https://kidde60.github.io/Awesome-books-with-ES6/',
+        source: 'https://github.com/kidde60/Awesome-books-with-ES6'
+
+    },
+    {
+        imagesrc: todolist,
+        title: 'Todo List web app ',
+        desc: 'This Todo list web app allows a user to add a todo, edit the todo, checkout the completed todo, delete a todo and delete all the completed todos.',
+        live: 'https://kidde60.github.io/To-Do-List-Webpage/',
+        source: 'https://github.com/kidde60/To-Do-List-Webpage'
+
+    }
+]
+export default CardContainer
