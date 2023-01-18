61f5eebb5ad531fc38460c432783764636501a22
kidde60 <kgeorgewilliam60.com>
2023-01-18 16:03:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './Home/Home';
-import About from './About/About';
-import Contact from './Contact/Contact';
-import Projects from './Projects/Projects';
-import { Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
-
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/About' element={<About />} />
-      <Route path='/Projects' element={<Projects />} />
-      <Route path='/Contact' element={<Contact />} />
-    </Routes>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
