c971059cc02a99d63e4d90fc78dea958329833f6
kidde60 <kgeorgewilliam60.com>
2023-01-18 14:45:55 +0300

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home/Home';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Projects from './Projects/Projects';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Projects' element={<Projects />} />
+      <Route path='/Contact' element={<Contact />} />
+    </Routes>
+
   );
 }
 
