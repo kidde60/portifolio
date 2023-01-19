3e3e477ae04f3e76102fd969085f671fdfa483fc
kidde60 <kgeorgewilliam60.com>
2023-01-19 18:13:35 +0300

    Add home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import {
-  FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter, FaGlobe,
-} from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+// import {
+//   FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter, FaGlobe,
+// } from 'react-icons/fa';
 // import Typical from 'react-typical';
-import Navbar from '../Navbar/Navbar';
+// import Navbar from '../Navbar/Navbar';
 import './Home.css';
 
-import { NavLink } from 'react-router-dom';
-
-export default function
-() {
+export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="home-container">
         <div className="home">
           <h1 className="coln">
-            Hello, I'M
+            Hello, Iam
             <span className="name">George William Kidde</span>
           </h1>
-          <div className="social">
 
-            <a href="https://kgeorgewilliam60@gmail.com"><FaEnvelope /></a>
-            <a href="https://github.com/kidde60"><FaGithub /></a>
-            <a href="https://www.linkedin.com/in/kidde-george-william/"><FaLinkedinIn /></a>
-            <a href="https://twitter.com/KGWilliam60"><FaTwitter /></a>
-            <a href=""><FaGlobe /></a>
-
-          </div>
           {/* <div className='primary-text'>
                         {' '}
                         <h1>
@@ -43,11 +33,11 @@ export default function
                             />
                         </h1>
                     </div> */}
-          <p className="paragraph">I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don't hestiate to contact me.</p>
+          <p className="paragraph">I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, do not hestiate to contact me.</p>
           <div className="home-btn">
             <NavLink to="../Contact" className="hire">Hire me</NavLink>
             <a href="Resume.pdf" download="Resume.pdf">
-              <button className="hire-btn">Get Resume</button>
+              <button type="button" className="hire-btn">Get Resume</button>
               {' '}
             </a>
           </div>
