c6042f11ffdc7d5de7dc68ce3d74521fa81af46a
kidde60 <kgeorgewilliam60.com>
2023-01-19 11:26:42 +0300

    EDIT

--- src/About/About.js
+++ src/About/About.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import Navbar from '../Navbar/Navbar'
-import './About.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import './About.css';
 
-export default function
-    () {
-    return (
-        <div className='about'>
-            <Navbar />
-            <h1 className='desc'> I'm a web developer who loves learning new skills and put my knowledge into building projects.
+export default function About() {
+  return (
+    <div className="about">
+      <Navbar />
+      <h1 className="desc">
+        Iam a web developer who loves learning new skills and put my
+        knowledge into building projects.
+        I can help you build a product , feature or website Look through
+        some of my work and experience! If you like what you see and have a
+        project you need coded, do not hestiate to contact me.
+      </h1>
+      <div className="home-btn">
+        <NavLink to="../Contact" className="hire-btn hire">Hire me</NavLink>
+        <a href="Resume.pdf" download="Resume.pdf">
+          <button type="button" className="hire-btn hire">Get Resume</button>
+          {' '}
+        </a>
+      </div>
 
-                I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.
-            </h1>
-            <div className='home-btn'>
-                <NavLink to='../Contact' className='hire-btn hire'>Hire me</NavLink>
-                <a href='Resume.pdf' download='Resume.pdf'><button className='hire-btn hire'>Get Resume</button> </a>
-            </div>
+    </div>
 
-        </div>
-
-
-    )
+  );
 }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
 import Contact from './Contact/Contact';
-import Projects from './Projects/Projects';
-import { Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
 
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/About' element={<About />} />
-      <Route path='/Projects' element={<Projects />} />
-      <Route path='/Contact' element={<Contact />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+
+      <Route path="/Contact" element={<Contact />} />
     </Routes>
 
   );
