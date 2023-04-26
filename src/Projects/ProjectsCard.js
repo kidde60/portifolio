ba5a086ee468cc0efe0781f6832663a4874ac352
kidde60 <kgeorgewilliam60@gmail.com>
2023-04-26 23:46:35 +0300

    update project

--- src/Projects/ProjectsCard.js
+++ src/Projects/ProjectsCard.js
@@ -4,27 +4,30 @@ import './Projects.css';
 
 export default function ProjectsCard() {
   return (
-    <div className="container">
-      {CardContainer.map((value) => (
-        <div
-          className="card"
-          key={value.title}
-        >
+    <div>
+      <h1 className="project">PROJECTS</h1>
+      <div className="container">
+        {CardContainer.map((value) => (
+          <div
+            className="card"
+            key={value.title}
+          >
 
-          <h1>{value.title}</h1>
-          <img src={value.imagesrc} alt="img" />
-          <p>{value.desc}</p>
-          <h3>
-            Built with :
-            {value.build}
-          </h3>
-          <div className="project-btn">
-            <a href={value.live} className="live btn">Live</a>
-            <a href={value.source} className="live">Source</a>
+            <h1>{value.title}</h1>
+            <img src={value.imagesrc} alt="img" />
+            <p>{value.desc}</p>
+            <h3>
+              Built with :
+              {value.build}
+            </h3>
+            <div className="project-btn">
+              <a href={value.live} className="live" id="btn">Live</a>
+              <a href={value.source} className="live">Source</a>
 
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
