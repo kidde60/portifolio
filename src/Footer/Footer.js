2a81b6219ba307d53028c67c611af2d886e6d254
kidde60 <kgeorgewilliam60@gmail.com>
2023-04-26 23:45:04 +0300

    Add footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter,
+} from 'react-icons/fa';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="social">
+        <a href="https://kgeorgewilliam60@gmail.com">
+          <FaEnvelope />
+        </a>
+        <a href="https://github.com/kidde60">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/kidde-george-william/">
+          <FaLinkedinIn />
+        </a>
+        <a href="https://twitter.com/KGWilliam60">
+          <FaTwitter />
+        </a>
+        <a href="https://george-william-kidde-portifolio.netlify.app/">
+          <i className="fa-solid fa-globe" />
+        </a>
+      </div>
+    </div>
   );
 }
