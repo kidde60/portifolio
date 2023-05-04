bd1d71d7bad62e6eafc6ed997a5d115496447d32
kidde60 <kgeorgewilliam60@gmail.com>
2023-05-04 19:38:36 +0300

    Add new tab to icons

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -9,19 +9,19 @@ export default function Footer() {
     <div className="footer">
       <div className="background" />
       <div className="social" id="footer-social">
-        <a href="https://kgeorgewilliam60@gmail.com">
+        <a href="mailto:kgeorgewilliam60@gmail.com" target="_blank" rel="noopener noreferrer">
           <FaEnvelope />
         </a>
-        <a href="https://github.com/kidde60">
+        <a href="https://github.com/kidde60" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/kidde-george-william/">
+        <a href="https://www.linkedin.com/in/kidde-george-william/" target="_blank" rel="noopener noreferrer">
           <FaLinkedinIn />
         </a>
-        <a href="https://twitter.com/KGWilliam60">
+        <a href="https://twitter.com/KGWilliam60" target="_blank" rel="noopener noreferrer">
           <FaTwitter />
         </a>
-        <a href="https://george-william-kidde-portifolio.netlify.app/">
+        <a href="https://george-william-kidde-portifolio.netlify.app/" target="_blank" rel="noopener noreferrer">
           <i className="fa-solid fa-globe" />
         </a>
       </div>
