cd3c649b696eb7f956f322503d1286517136d1c0
kidde60 <kgeorgewilliam60@gmail.com>
2024-10-12 10:44:28 +0300

    Add projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import CardContainer from './ProjectsDetails';
-import './Projects.css';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import React from "react";
+import CardContainer from "./ProjectsDetails";
+import "./Projects.css";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function ProjectsCard() {
   return (
@@ -11,24 +11,35 @@ export default function ProjectsCard() {
       <h1 className="project">PROJECTS</h1>
       <div className="container">
         {CardContainer.map((value) => (
-          <div
-            className="card"
-            key={value.title}
-          >
+          <div className="card" key={value.title}>
             <img src={value.imagesrc} alt="img" />
             <div className="card-container">
               <h1>{value.title}</h1>
 
               <p>{value.desc}</p>
               <ul className="lists d-flex flex-row justify-content-start">
-
                 <li className="css">{value.build.html}</li>
                 <li>{value.build.bootstrap}</li>
                 <li>{value.build.lags}</li>
               </ul>
               <div className="project-btn">
-                <a href={value.live} className="live" id="btn">Live</a>
-                <a href={value.source} className="live">Source</a>
+                <a
+                  href={value.live}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="live"
+                  id="btn"
+                >
+                  Live
+                </a>
+                <a
+                  href={value.source}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="live"
+                >
+                  Source
+                </a>
               </div>
             </div>
           </div>
