222fdb5bd969b807b551b76a5bb0d395f5a5ed8a
kidde60 <kgeorgewilliam60@gmail.com>
2024-10-12 10:42:00 +0300

    Update footer page

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  FaEnvelope, FaGithub, FaLinkedinIn, FaTwitter,
+} from 'react-icons/fa';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="background" />
+      <div className="social" id="footer-social">
+        <a href="mailto:kgeorgewilliam60@gmail.com">
+          <FaEnvelope />
+        </a>
+        <a href="https://github.com/kidde60" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/kidde-george-william/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedinIn />
+        </a>
+        <a href="https://twitter.com/KGWilliam60" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://george-william-kidde-portifolio.netlify.app/" target="_blank" rel="noopener noreferrer">
+          <i className="fa-solid fa-globe" />
+        </a>
+      </div>
+    </div>
+  );
+}
